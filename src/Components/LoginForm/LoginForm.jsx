9d4bdda736844b878bf3cf660c9772e4ce2114ef
koek1 <geandredupreez1@gmail.com>
2024-08-08 13:25:21 +0200

    Changes to be committed: 	modified:   src/Components/LoginForm/LoginForm.jsx 	modified:   src/Components/LoginForm/LoginFormStyle.css

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginFormStyle.css';
-import { FaUser, FaLock, FaRegEye, FaRegEyeSlash} from "react-icons/fa";
+import { FaUser, FaLock, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const LoginForm = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <div className='wrapper'>
             <form action="">
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username'required/>
+                    <input type="text" placeholder='Username' required />
                     <FaUser className='icon' />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Password'required/>
-                    <FaRegEye className='eye_icon' />
+                    <input 
+                        type={passwordVisible ? "text" : "password"} 
+                        placeholder='Password' 
+                        required 
+                    />
+                    <span onClick={togglePasswordVisibility} className='eye_icon'>
+                        {passwordVisible ? <FaRegEyeSlash /> : <FaRegEye />}
+                    </span>
                     <FaLock className='icon' />
                 </div>
 
                 <div className="remember-forgot">
-                    <label><input type="checkbox" />Remenber Me</label>
+                    <label><input type="checkbox" /> Remember Me</label>
                     <a href="#">Forgot Password ?</a>
                 </div>
 
                 <button type="submit">Login</button>
 
                 <div className="register-link">
-                    <p>Don't have an account ?<a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#">Register</a></p>
                 </div>
-
-
             </form>
         </div>
     );
