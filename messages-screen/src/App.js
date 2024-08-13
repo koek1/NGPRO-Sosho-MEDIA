95fd462858230e6e96b438c0e8ac32e4ab20e9de
Geandre <80062438+koek1@users.noreply.github.com>
2024-08-13 11:44:47 +0200

    Entire (Current Project)
    This is the current project Note: Changes are still being made and optimisation is underway

---
+++ messages-screen/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MessagesDash from './MessagesDash.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MessagesDash />
+      </header>
+    </div>
+  );
+}
+
+export default App;
